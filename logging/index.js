d68a861c8378905ca7fc8f7eddbe1281d63ecac3
SirAglovale <SirAglovaleOfTheRoundTable@gmail.com>
2017-09-15 22:20:48 +0100

    Error color red

--- logging/index.js
+++ logging/index.js
@@ -13,5 +13,5 @@ global.error = (eval) => {
     if(eval instanceof Object) {
         eval = JSON.stringify(eval);
     }
-    process.stderr.write(`${global._utils.application.name || 'application'}@${global._utils.application.version || 'version'}[${current_time}] -> ${eval}\n`);
+    process.stderr.write(`\x1b[31m${global._utils.application.name || 'application'}@${global._utils.application.version || 'version'}[${current_time}] -> ${eval}\x1b[0m\n`);
 }
